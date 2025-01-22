2430e53ed23b3650a83b701824b3ec9c23cc44b8
Roddy192 <149844471+Roddy192@users.noreply.github.com>
2025-01-22 17:06:37 -0300

    update 22-01-25
    adicionei funcao de add usuario (incompleto)

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,12 +1,23 @@
 import express from "express";
 import cors from "cors";
 const app = new express();
+app.use(express.json());
+app.use (
+  cors({
+    origin: 'http://localhost:5500',
+    methods: ['GET', 'POST', 'PUT','DELETE'],
+    allowedHeaders: ['Content-Type'],
+
+  })
+);
 import con from './connection.js'
 
-app.use(cors({origin:'http://127.0.0.1:5500/' }))
+app.use(cors({origin:true, credentials: true }))
 
 
-app.get('/', (req, res) => {
+
+
+app.get('/get', (req, res) => {
     con.query('SELECT * FROM contatos', (err, result) => {
       if (err) {
         res.send(err)
@@ -16,6 +27,12 @@ app.get('/', (req, res) => {
       res.send(result);
     })
 });
+
+app.post('/add', (req, res) => {
+  console.log('Olá Mundo');
+  res.send("funcionando");
+
+    });
 
 app.listen(3000, ()=> console.log('Server running on port 3000'));
 
