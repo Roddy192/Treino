a959a42900071de01143ce958e03f0b35226e897
Roddy192 <149844471+Roddy192@users.noreply.github.com>
2025-01-30 23:04:43 -0300

    update 2 30-01-25
    Função de adicionar contato finalizada

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,7 +4,7 @@ const app = new express();
 app.use(express.json());
 app.use (
   cors({
-    origin: 'http://localhost:5500',
+    origin: '*',
     methods: ['GET', 'POST', 'PUT','DELETE'],
     allowedHeaders: ['Content-Type'],
 
@@ -13,6 +13,8 @@ app.use (
 import con from './connection.js'
 
 app.use(cors({origin:true, credentials: true }))
+
+
 
 
 
@@ -29,9 +31,12 @@ app.get('/get', (req, res) => {
 });
 
 app.post('/add', (req, res) => {
-  console.log('Olá Mundo');
-  con.query('INSERT INTO contatos (nome, telefone, email) VALUES () ')
-  res.send(req.body);
+  const nome = req.body.nome;
+  const email = req.body.email;
+  const telefone = req.body.telefone;
+  con.query('INSERT INTO contatos (nome, email, telefone) VALUES (?, ?, ?)', [nome, email, telefone]);
+  console.log(req.body);
+  console.log(nome);
     });
 
 app.listen(3000, ()=> console.log('Server running on port 3000'));
